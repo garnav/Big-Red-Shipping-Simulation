14b178b04f42aa556eb8290065c146435b7b90f4
Talha Baig <tbaig6@gmail.com>
2017-04-24 16:04:42 -0400

    added enum for dorms and also added more skeleton code

--- src/Map.java
+++ src/Map.java
@@ -5,4 +5,37 @@ public class Map {
 	 * The issue that arises is representing the information (ie: time, distance etc.) 
 	 * of how to go from one place to the next.
 	 */
+	
+	public static int getCost(Dorm a, Dorm b){
+		return Cost();
+	}
+	
+	public class Cost{
+		Dorm a;
+		Dorm b;
+		int time;
+		int distance;
+		int fuel;
+	}
+	
+	public enum Dorm {
+		CLARA_DICKSON,
+		LOW_RISE_6,
+		LOW_RISE_7,
+		UJAMAA,
+		HILC,
+		JAM,
+		HIGH_RISE_5,
+		AKWEKON,
+		TOWNHOUSE,
+		MEWS,
+		MARY_DONLAN,
+		BALCH,
+		COURT_KAY_BAUER,
+		HIGH_RISE_5,
+		JAMESON,
+		ECOLOGY_HOUSE,
+		LLC,
+		RISLEY
+	}
 }
