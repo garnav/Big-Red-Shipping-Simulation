10e6efe5be9ba8db523e466eb20537cce87be399
Arnav Ghosh <garnav@gmail.com>
2017-04-24 15:33:41 -0400

    Updated comments and skelal for ttrucks

--- src/Truck.java
+++ src/Truck.java
@@ -2,12 +2,22 @@
 public class Truck {
 	//incomplete description so far
 	
-	int fuel;
-	int remSpace;
-	String currentLocation; 
+	private int remfuel;
+	private int remSpace;
+	private int maxWeight;
+	private int currWeight;
+	private String currentLocation; 
 	
 	public Truck(int startFuel, String startLoc, int startSpace){
 		
+	}
+	
+	/*only returns true iff the truck is physically capable to
+	 * move to the given location, taking into account
+	 * given fuel and connection constraints.
+	*/
+	public boolean moveLoc(){
+		return false;
 	}
 	
 	
